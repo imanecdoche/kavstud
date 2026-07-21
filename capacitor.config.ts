313d7081be68098dc15cf82304a067397366311e
imanecdoche <119998262+imanecdoche@users.noreply.github.com>
2026-07-22 00:48:48 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kavioedu.app',
+  appName: ' KAVIO Edu',
+  webDir: 'dist'
+};
+
+export default config;
